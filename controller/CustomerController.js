9aca992efa0d99f23eeb1d1c20f3eaa592453f0b
Keith Prakash <71426291+KeithPrakash@users.noreply.github.com>
2023-02-13 10:13:51 +0530

    save customer

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -1,19 +1,11 @@
 const CustomerSchema = require('..model/Customer');
-
 const saveCustomer=(req,res)=>{
-    const customer= new CustomerSchema({
-        name:req.body.name,
-        address:req.body.address,
-        salary:req.body.salary
-    });
-    customer.save().then(result=>{
-        res.json({data:{status:201,message:'customer saved'}})
-    }).catch(error=>{
-        console.log(error);
-        res.json(error);
-    })
+const customer = CustomerSchema({
+  name: req.body.name,
+  address: req.body.address,
+  salary: req.body.salary,
+});
 }
-
 
 module.exports={
     saveCustomer
