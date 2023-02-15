3b6562b28ce255241fdae4dcb814669f70b8bdf8
Keith Prakash <71426291+KeithPrakash@users.noreply.github.com>
2023-02-15 16:28:00 +0530

    6.2 encrypt password

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,22 +1,31 @@
 const UserSchema = require("../model/User");
+const bcrypt = require('bcrypt');
+
 
 const signup = (req, res) => {
-  const user = new UserSchema({
-    email: req.body.email,
-    password: req.body.password,
-    fullName: req.body.fullName,
-  });
 
   UserSchema.findOne( {email:req.body.email}).then(existsData=>{
     if(existsData===null){
-        user.save()
-    .then((result) => {
-      res.json({ data: { status: 201, message: "Registered new user", result } });
-    })
-    .catch((error) => {
-      console.log(error);
-      res.json(error);
-    })
+        bcrypt.hash(req.body.password,10, function (err, hash) {
+                     const user = new UserSchema({
+                       email: req.body.email,
+                       password: hash,
+                       fullName: req.body.fullName,
+                     });
+
+           user
+             .save()
+             .then((result) => {
+               res.json({
+                 data: { status: 201, message: "Registered new user", result },
+               });
+             })
+             .catch((error) => {
+               console.log(error);
+               res.json(error);
+             });
+        });
+
 
     }else{
        res.status(403).json({data:{status:403, message:"Already Exits"}}) 
